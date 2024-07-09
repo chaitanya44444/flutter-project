e462e51ea39eca30dc40a20d0977b08e143cf27b
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-07-09 21:55:43 +0530

    arcade

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.black,
+        body: Container(
+          color: Colors.white,
+          child: Text("Hello")
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
