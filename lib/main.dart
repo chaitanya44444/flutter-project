b81b5b40860f6f4071fd12e5267ff2a7da437f43
Chaitanya Vaswani <vaswanichaitanya@gmail.com>
2024-07-10 21:40:38 +0530

    ytessir

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: Container(
-          color: Colors.white,
-          child: Text("Hello")
+        body: SafeArea(
+          child: Container(
+            height:100,
+            width: 100,
+            margin:EdgeInsets.only(left:50),
+            padding: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text("Hello")
+          ),
         ),
       ),
     );
